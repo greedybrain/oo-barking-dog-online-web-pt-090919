98a2bf7a035d445ce92a984332d06e7ebb50aa97
Naya  Willis <greedybrain18@gmail.com>
2019-09-21 18:05:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,15 @@
 class Dog 
   
   def name=(dog_name) # setter
-    
+    @dog_name = dog_name
   end
   
   def name # getter
-    
+    @dog_name
+  end
+  
+  def bark
+    puts "woof!"
   end
   
 end
